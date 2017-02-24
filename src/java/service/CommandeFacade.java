922c02d22cb8ccd2a0a11f51f7d32de29bd5dd71
WAFA <WAFA@PC-Toshiba.Home>
2017-02-24 22:33:56 +0000

    initialisation du projet

---
+++ src/java/service/CommandeFacade.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import bean.Commande;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author WAFA
+ */
+@Stateless
+public class CommandeFacade extends AbstractFacade<Commande> {
+
+    @PersistenceContext(unitName = "gestionStockInventairePU")
+    private EntityManager em;
+    
+
+    
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CommandeFacade() {
+        super(Commande.class);
+    }
+    
+}
